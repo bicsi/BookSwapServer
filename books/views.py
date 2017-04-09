54f4427575426c37aa29d61a32a36c8d17938526
Bicsi <bicsi@ymail.com>
2017-04-09 22:24:13 +0300

    Pre-branch

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 # Create your views here
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from . import serializers, models
@@ -21,7 +22,18 @@ class ReviewListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         return self.queryset.filter(book=self.kwargs.get('book_pk'))
 
+    def perform_create(self, serializer):
+        book = get_object_or_404(models.Book, pk=self.kwargs.get('book_pk'))
+        serializer.save(book=book)
+
 
 class ReviewRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            book_id=self.kwargs.get('book_pk'),
+            pk=self.kwargs.get('pk')
+        )
